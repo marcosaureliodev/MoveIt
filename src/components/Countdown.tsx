d94f5e733a10d1ec35da7170eedabfc8ff437baf
marcos aurelio <marcos.aureliodev@gmail.com>
2021-02-25 21:33:37 -0300

    🤓 countdown next.js

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from '../styles/components/Countdown.module.css'
 
 export function Countdown() {
-    const [time, setTime] = useState (25 * 60);
+    const [time, setTime] = useState (1 * 60);
+    const [active, setActive] = useState (false);
 
     const minutes = Math.floor(time / 60);
-    const seconds =time % 60;
+    const seconds = time % 60;
 
     const [minuteLeft, minutRight] = String(minutes).padStart(2, '0').split('');
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown() {
-
+        setActive(true);
     }
+
+    useEffect(() => {
+        if (active && time > 0) {
+            setTimeout(() => {
+                setTime(time - 1);
+            }, 1000)
+        }
+    }, [active, time])
 
     return (
         <div>
